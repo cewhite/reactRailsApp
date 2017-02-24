18b6a83f88ed442dea4391eb0919faf68417df7c
cresta white <crestawhite@gmail.com>
2017-02-23 20:53:40 -0500

    working

--- app/assets/javascripts/components/_new_item.js.jsx
+++ app/assets/javascripts/components/_new_item.js.jsx
@@ -1,21 +1,28 @@
-// var NewItem= React.createClass({
-//   render() {
-//    return (
-//     <div>
-//     <h1>new item</h1>
-//     </div>
-//      )
-//       }
-//     });
+var NewItem= React.createClass({
+  render() {
+   return (
+    <div>
+    <h1>new item</h1>
+    </div>
+     )
+      }
+    });
 var NewItem= React.createClass({
   render() {
     return (
       <div>
        <input ref='name' placeholder='Enter the name of the item' />
         <input ref='description' placeholder='Enter a description' />
-        <button>Submit</button>
+       <button onClick={this.handleClick}>Submit</button>
         </div>
          )
-          )
-           }
-            });
+          }
+           });
+// app/assets/javascripts/components/_new_item.js.jsx
+// var NewItem = …
+handleClick() {
+ var name = this.refs.name.value;
+  var description = this.refs.description.value;
+   console.log('The name value is ' + name + 'the description value is ' + description);
+    },
+     //render()..
