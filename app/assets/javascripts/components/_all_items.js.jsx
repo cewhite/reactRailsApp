62b1e47b6bd38a14fcc214b71d7d6ac8dec49211
cresta white <crestawhite@gmail.com>
2017-02-23 21:52:58 -0500

    updated files

--- app/assets/javascripts/components/_all_items.js.jsx
+++ app/assets/javascripts/components/_all_items.js.jsx
@@ -1,31 +1,20 @@
 var AllItems = React.createClass({
-  componentDidMount() {
-    console.log('Component mounted');
-  }, render() {
+ render() {
+  var items= this.props.items.map((item) => {
    return (
+    <div key={item.id}>
+     <h3>{item.name}</h3>
+     <p>{item.description}</p>
+     </div>
+     )
+      });
+  return(
     <div>
-    <h1>All items component</h1>
+    {items}
      </div>
       )
        }
         });
-
-
-// render() {
-  var items= this.state.items.map((item) => {
-    return (
-     <div key={item.id}>
-      <h3>{item.name}</h3>
-      <p>{item.description}</p>
-       </div>
-       )
-
-  return(
-        <div>
-         {items}
-         </div>
-         )
-          });
 
 
 
--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -1,29 +1,20 @@
 var Body = React.createClass({
- render(){
-  return (
-   <div>
-   <NewItem />
-    <AllItems />
-    </div>
-    )
-     }
-     });
-var AllItems = React.createClass({
- getInitialState() {
-  return { items: [] }
-
-},
-
-componentDidMount() {
-  $.getJSON('/api/v1/items.json', (response) => { this.setState({ items: response }) });
-}
-});
- <AllItems items={this.state.items} />
-var items= this.props.items.map((item) => {
-   handleSubmit(item)
-    var newState = this.state.items.concat(item);
-    this.setState({ items: newState })
-
-    });
-
-    <NewItem handleSubmit={this.handleSubmit}/>
+  getInitialState() {
+   return { items: [] }
+    },
+     componentDidMount() {
+      $.getJSON('/api/v1/items.json', (response) => { this.setState({ items: response }) });
+       },
+        handleSubmit(item) {
+          var newState = this.state.items.concat(item);
+           this.setState({ items: newState })
+            },
+             render() {
+              return (
+               <div>
+                <NewItem handleSubmit={this.handleSubmit}/>
+                 <AllItems items={this.state.items} />
+                  </div>
+                   )
+                    }
+                     });
